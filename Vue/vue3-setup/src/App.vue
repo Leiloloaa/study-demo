5bfbc04f635f1ec08835888bf69f380fb64c1376
cl1997 <Leiloloaa@gmail.com>
2022-02-23 10:39:43 +0800

    vue3-setup 语法糖练习

--- Vue/vue3-setup/src/App.vue
+++ Vue/vue3-setup/src/App.vue
@@ -1,45 +1,63 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { ref, watch, onMounted } from 'vue';
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref, watch, onMounted } from "vue";
+import StyleBind from "./components/StyleBind.vue";
 
-const msg = ref('456')
-const show = ref(false)
-const childRefs = ref<{ a: string, showLoveYou: () => void } | null>(null)
+const msg = ref("456");
+const show = ref(false);
+const childRefs = ref<{ a: string; showLoveYou: () => void } | null>(null);
 
 onMounted(() => {
   console.log(childRefs.value?.a);
   childRefs.value?.showLoveYou();
-})
+});
 
 function showMsg() {
-  msg.value = '错误 500'
-  changeShow(true)
+  msg.value = "错误 500";
+  changeShow(true);
 }
 
 function changeShow(target = false) {
-  show.value = target
+  show.value = target;
 }
 
 watch(
   () => show.value,
   (val, prevVal) => {
-    console.log(`show is: ` + val)
+    console.log(`show is: ` + val);
   }
-)
+);
+
+// 顶层 await
+// 使用了 await setup 函数 会自动加上 async
+const post = await fetch(
+  `https://api.uomg.com/api/get.kg?songurl=https://node.kg.qq.com/play?s=YaCv8EYfJunVWYcH`
+).then((r) => r.json());
+
+console.log(post);
+
+// style module
+// const css = useCssModule();
+// console.log(css);
+
+// 状态驱动 css
+// const color = ref("red");
+
+// setTimeout(() => {
+//   color.value = "blue";
+// }, 2000);
 </script>
 
 <template>
   <button @click="showMsg">showMsg</button>
   <HelloWorld :msg="msg" :isShow="show" @changeShow="changeShow" ref="childRefs" />
+
+  <p>状态驱动 css</p>
+  <StyleBind />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+/* p {
+  color: v-bind(color);
+} */
 </style>
---
+++ Vue/vue3-setup/src/components/StyleBind.vue
@@ -0,0 +1,15 @@
+<script setup>
+const theme = {
+  color: "red",
+};
+</script>
+
+<template>
+  <p>hello 123</p>
+</template>
+
+<style scoped>
+p {
+  color: v-bind("theme.color");
+}
+</style>
